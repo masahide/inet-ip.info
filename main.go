933d4c2b0a3d46ab4d3dc8d85582b389d1780f30
YAMASAKI Masahide <masahide.y@gmail.com>
2015-03-21 22:32:57 +0900

    / ->ip\n , /ip -> ip , /json -> json /json/indent -> json indent

--- main.go
+++ main.go
@@ -8,8 +8,10 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", ip)
+	http.HandleFunc("/", root)
+	http.HandleFunc("/ip", ip)
 	http.HandleFunc("/json", jsonPrint)
+	http.HandleFunc("/json/indent", jsonIndentPrint)
 	fmt.Println("listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
@@ -17,11 +19,22 @@ func main() {
 	}
 }
 
+func root(res http.ResponseWriter, req *http.Request) {
+	ip(res, req)
+	res.Write([]byte("\n"))
+}
 func ip(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "%s", req.Header["X-Forwarded-For"])
+	ips := req.Header["X-Forwarded-For"]
+	length := len(ips)
+	fmt.Fprintf(res, "%s", ips[length-1])
 }
 
 func jsonPrint(res http.ResponseWriter, req *http.Request) {
+	j, _ := json.Marshal(req)
+	fmt.Fprintln(res, string(j))
+}
+
+func jsonIndentPrint(res http.ResponseWriter, req *http.Request) {
 	j, _ := json.MarshalIndent(req, "", " ")
 	fmt.Fprintln(res, string(j))
 }
